346cb44ed5d16f300ef81c6d8de0fb5a5c52cbdb
Gavin <gurven-mckruger@hotmail.co.uk>
2024-07-31 16:05:45 +0100

    Custom icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd8df42b64fd94c6edd31');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '08213a57324beed6a370');
